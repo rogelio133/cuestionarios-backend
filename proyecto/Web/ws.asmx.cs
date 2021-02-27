29d11b2dce7f49165a4b7fc0e848dbd032e0dfc3
Rogelio Pacheco Elorza <rogelio_133@outlook.com>
2021-02-26 20:30:13 -0600

    creación del proyecto

---
+++ proyecto/Web/ws.asmx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Web
+{
+    /// <summary>
+    /// Descripción breve de ws
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+     [System.Web.Script.Services.ScriptService]
+    public class ws : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public object ValidateLogin(string user, string password)
+        {
+            object response = null;
+            try
+            {
+                if(user == "q@a.com" && password=="123")
+                {
+                    response = new
+                    {
+                        success = true,
+                        data = new  {
+                            name = "Rogelio 133",
+                            lastName = "Pacheco Elorza",
+                            token = "123456789",
+                        },
+                        
+                        message = "Datos incorrectos"
+                    };
+                }
+                else
+                {
+                    response = new
+                    {
+                        success = false,
+                        message = "Datos incorrectos"
+                    };
+                }
+
+               
+            }
+            catch (Exception ex)
+            {
+                response = new {
+                    success = false,
+                    message = "No se pudo procesar su solicitud"
+                };
+                 
+            }
+
+            return response;
+        }
+    }
+}
